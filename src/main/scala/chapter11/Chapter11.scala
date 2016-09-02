5d88ebb629d341dc96455f39fa70dc3bc064fa0b
Luis Rodero-Merino <luis.rod@gmail.com>
2016-09-02 20:52:27 +0200

    More exercises of Chapter 11

--- src/main/scala/chapter11/Chapter11.scala
+++ src/main/scala/chapter11/Chapter11.scala
@@ -26,20 +26,50 @@ trait Monad[M[_]] extends Functor[M] {
 }
 
 object Exercise_11_3 {
-  def sequence[F[_]: Monad, A](la: List[F[A]]): F[List[A]] = {
+  def sequence[F[_]: Monad,A](la: List[F[A]]): F[List[A]] = {
     val monad = implicitly[Monad[F]]
     la match {
-      case Nil => monad.unit(List[A]())
+      case Nil => monad.unit(List.empty[A])
       case fa :: tail => monad.flatMap(sequence(tail)){ la =>
         monad.map(fa)(a => a :: la)
       }
     }
   }
-  def traverse[F[_]: Monad, A, B](la: List[A])(f: A => F[B]): F[List[B]] =
+  def traverse[F[_]: Monad,A,B](la: List[A])(f: A => F[B]): F[List[B]] =
     sequence(la map f)
 }
 
-case class Id[A](a: A)
+object Exercise_11_4 {
+  def replicateM[F[_]: Monad,A](n: Int, ma: F[A]): F[List[A]] =
+    Exercise_11_3.sequence(List.fill[F[A]](n)(ma))
+}
+
+object Exercise_11_7 {
+  def compose[F[_]: Monad,A,B,C](f: A => F[B])(g: B => F[C]): A => F[C] =
+    (a:A) => { 
+      val monad = implicitly[Monad[F]]
+      monad.flatMap(f(a))(g)
+    }
+}
+
+object Exercise_11_12 {
+  def join[F[_]: Monad,A](mma: F[F[A]]): F[A] = {
+    val monad = implicitly[Monad[F]]
+    monad.flatMap(mma){ma => monad.flatMap(ma)(a => monad.unit(a))}
+  }
+}
+
+object Exercise_11_13 {
+  def flatMap2[F[_]: Monad,A,B](ma: F[A])(f: A => F[B]): F[B] = {
+    val monad = implicitly[Monad[F]]
+    Exercise_11_12.join(monad.map(ma)(f))
+  }
+}
+
+case class Id[A](a: A) {
+  def map[B](f: A => B): Id[B] = Monad.idMonad.map(this)(f)
+  def flatMap[B](f: A => Id[B]): Id[B] = Monad.idMonad.flatMap(this)(f)
+}
 
 case class State[S,A](run: S => (A,S)) {
   def map[B](f: A => B): State[S,B] = State{ (s:S) =>
@@ -55,20 +85,32 @@ case class State[S,A](run: S => (A,S)) {
 
 object Monad {
 
-  def idMonad = new Monad[Id] {
+  val idMonad = new Monad[Id] {
     def unit[A](a: A) = Id(a)
     def flatMap[A,B](ia: Id[A])(f: A => Id[B]): Id[B] = ia match {
       case Id(a) => f(a)
     }
   }
 
+  object IdMonad extends Monad[Id] {
+    def unit[A](a:A) = Id(a)
+    def flatMap[A,B](ia: Id[A])(f: A => Id[B]): Id[B] = ia match {
+      case Id(a) => f(a)
+    }
+  }
+  
   type IntState[A] = State[Int,A]
 
-  def intStateMonad = new Monad[IntState] {
+  val intStateMonad = new Monad[IntState] {
     def unit[A](a:A): IntState[A] = State{s => (a,s)}
     def flatMap[A,B](sa: IntState[A])(f: A => IntState[B]): IntState[B] = sa flatMap f
   }
 
+  trait StateMonad[S] extends Monad[({type f[x] = State[S,x]})#f] {
+    def unit[A](a: => A): State[S,A] = State(s => (a,s))
+    def flatMap[A,B](st: State[S,A])(f: A => State[S,B]): State[S,B] = 
+      st flatMap f
+  }
 
 }
 
